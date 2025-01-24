4a67be64b4bc93fbe35a2550eeed3fbc16a803b1
ShuhengW <wsh9487@gmail.com>
2025-01-24 11:58:38 -0600

    updated

--- dice.rb
+++ dice.rb
@@ -9,13 +9,19 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
+get("/") do
+
+  erb(:elephant)
+
+end
+
+
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six, {:layout=> :wrapper})
 end
