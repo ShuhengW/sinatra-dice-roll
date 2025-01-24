cb72e8916f40cc0a0528a6c69c73b40a7527eb1a
ShuhengW <wsh9487@gmail.com>
2025-01-24 12:33:03 -0600

    update

--- dice.rb
+++ dice.rb
@@ -17,11 +17,62 @@ end
 
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = [] 
+  2.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+  
+  #sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  # @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_six, {:layout=> :wrapper})
+  erb(:two_six)
+end
+
+get("/dice/2/10") do
+  @rolls = [] 
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
+  
+  erb(:two_ten)
+end
+
+
+get("/dice/1/20") do
+  @rolls = [] 
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
+
+  #@outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = [] 
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
+
+  #@outcome = "You rolled a #{@die}."
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
